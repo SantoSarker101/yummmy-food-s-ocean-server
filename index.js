ddf721dca4ce24178809e6d85784b6ea1ff71e34
SantoSarker101 <santosarker404@gmail.com>
2023-11-07 02:58:15 +0600

    install cors and import cors

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+
+
+app.use(cors());
 
 app.get('/', (req,res) =>{
 	res.send('yummmy-foods-ocean is running')
 });
+
+
 
 app.listen(port,() =>{
 	console.log((`yummmy-food's-ocean is running on port: ${port}`));
